393043045d6cdc464cbf4f18a319beffd24c82e9
Raynor Elgie <elgieraynor@gmail.com>
2026-03-12 22:46:53 +0000

    register

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,3 +15,12 @@ import os
 tests_integration_dir = os.path.join(os.path.dirname(__file__), "integration")
 if tests_integration_dir not in sys.path:
     sys.path.insert(0, tests_integration_dir)
+
+
+def pytest_configure(config):
+    """
+    Register custom marks to avoid PytestUnknownMarkWarning.
+    """
+    config.addinivalue_line(
+        "markers", "slow: marks tests as slow (deselect with '-m \"not slow\"')"
+    )
